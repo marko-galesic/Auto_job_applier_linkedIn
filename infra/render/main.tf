5cab7e3df0df74f7ede5a7d1efbc7139828c65eb
Marko Galesic <mf.galesic@gmail.com>
2025-12-06 08:12:44 -0800

    Fix render web service config for terraform

--- infra/render/main.tf
+++ infra/render/main.tf
@@ -19,6 +19,10 @@ resource "render_web_service" "auto_job_ui" {
   plan   = var.service_plan
   region = var.region
 
+  env           = "python"
+  build_command = "pip install --upgrade pip && pip install -r requirements.txt"
+  start_command = "gunicorn app:app --bind 0.0.0.0:$PORT"
+
   runtime_source = {
     type = "REPO"
 
@@ -27,12 +31,6 @@ resource "render_web_service" "auto_job_ui" {
       url         = var.repo_url
       auto_deploy = true
     }
-  }
-
-  service_details {
-    env           = "python"
-    build_command = "pip install --upgrade pip && pip install -r requirements.txt"
-    start_command = "gunicorn app:app --bind 0.0.0.0:$PORT"
   }
 
   env_vars = {
